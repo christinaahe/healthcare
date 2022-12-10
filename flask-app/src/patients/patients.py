5634ee67ebd84533d11e237aad0a2759a37788f3
Eunice Koo <98850825+euniceseo@users.noreply.github.com>
2022-12-10 01:18:00 -0500

    updated patients routes

--- flask-app/src/patients/patients.py
+++ flask-app/src/patients/patients.py
@@ -6,13 +6,7 @@ from src import db
 
 patients = Blueprint('patients', __name__)
 
-"""
-@patients.route('/')
-def default_patient():
-    return '<h1>it works</h1>'
-"""
-
-#  Get all the patients from the database
+#  Get all the patient record
 @patients.route('/record/<patientID>', methods=['GET'])
 def get_records(patientID):
     # get a cursor object from the database
@@ -20,6 +14,90 @@ def get_records(patientID):
 
     # use cursor to query the database for a list of products
     cursor.execute('select * from record where patientID = {0}'.format(patientID))
+
+     # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+     # create an empty dictionary object to use in
+     # putting column headers together with data
+    json_data = []
+
+     # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+     # for each of the rows, zip the data elements together with
+     # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+        the_response = make_response(jsonify(json_data))
+        the_response.status_code = 200
+        the_response.mimetype = 'application/json'
+    return the_response
+
+#  Get all the patient information
+@patients.route('/patientinfo/<patientID>', methods=['GET'])
+def get_patient_info(patientID):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('select * from patient where patientID = {0}'.format(patientID))
+
+     # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+     # create an empty dictionary object to use in
+     # putting column headers together with data
+    json_data = []
+
+     # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+     # for each of the rows, zip the data elements together with
+     # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+        the_response = make_response(jsonify(json_data))
+        the_response.status_code = 200
+        the_response.mimetype = 'application/json'
+    return the_response
+
+#  Get all the patient condition from the database
+@patients.route('/condition/<patientID>', methods=['GET'])
+def get_condition(patientID):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('select * from patient_condition where patientID = {0}'.format(patientID))
+
+     # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+     # create an empty dictionary object to use in
+     # putting column headers together with data
+    json_data = []
+
+     # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+     # for each of the rows, zip the data elements together with
+     # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+        the_response = make_response(jsonify(json_data))
+        the_response.status_code = 200
+        the_response.mimetype = 'application/json'
+    return the_response
+
+#  Get all the patient test results from the database
+@patients.route('/tests/<patientID>', methods=['GET'])
+def get_tests(patientID):
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('select * from test where patientID = {0}'.format(patientID))
 
      # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
@@ -53,9 +131,22 @@ def get_appointments(patientID):
     return jsonify(json_data)
 
 @patients.route('/availability/<physicianID>', methods=['GET'])
-def get_availabilities(physicianID):
+def get_availabilities_physician(physicianID):
     cursor = db.get_db().cursor()
     cursor.execute('select * from availability where physicianID = {0}'.format(physicianID))
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+# gets a list of patientIDS
+@patients.route('/patientID', methods=['GET'])
+def get_patientID():
+    cursor = db.get_db().cursor()
+    cursor.execute('select patientID from patient')
     column_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
