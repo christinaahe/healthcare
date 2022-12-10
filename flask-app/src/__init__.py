7ce97a4bf1bd9361d03c9832f9f4223bf7c53521
christinaahe <119445668+christinaahe@users.noreply.github.com>
2022-12-10 10:09:21 -0500

    Update __init__.py

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -19,12 +19,11 @@ def create_app():
     app.config['MYSQL_DATABASE_USER'] = 'webapp'
 
     # this doesn't work
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'abc123'
+    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_password.txt').readline()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
     app.config['MYSQL_DATABASE_DB'] = 'healthcare'  # Change this to your DB name
 
-    # Initialize the database object with the settings above.
     db.init_app(app)
 
     # Import the various routes
