6bc28b79183f11057b2c2cd15fe92b349772780e
gracewang <wang.gra@northeastern.edu>
2022-12-12 11:25:55 -0500

    Update physicians.py

--- flask-app/src/physicians/physicians.py
+++ flask-app/src/physicians/physicians.py
@@ -6,47 +6,6 @@ from src import db
 
 physicians = Blueprint('physicians', __name__)
 
-# Get all the patients from the database
-@physicians.route('/patients', methods=['GET'])
-def get_patients():
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-    #current_app.logger.info("Hello")
-    #current_app.logger.info(current_app.url_map)
-    # use cursor to query the database for a list of products
-    cursor.execute('select * from patient')
-
-    # grab the column headers from the returned data
-    column_headers = [x[0] for x in cursor.description]
-
-    # create an empty dictionary object to use in 
-    # putting column headers together with data
-    json_data = []
-
-    # fetch all the data from the cursor
-    theData = cursor.fetchall()
-
-    # for each of the rows, zip the data elements together with
-    # the column headers. 
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-    
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-@physicians.route('/appointment/<physicianID>', methods=['GET'])
-def get_appointments(physicianID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from appointment where physicianID = {0}'.format(physicianID))
-    column_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
 
 @physicians.route('/editavailability', methods=['POST'])
 def add_availability():
@@ -73,23 +32,6 @@ def get_availabilities():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
-
-# Get customer detail for customer with particular userID
-
-@physicians.route('/<physicianID>', methods=['GET'])
-def get_customer(physicianID):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from physician where physicianID = {0}'.format(physicianID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-        the_response = make_response(jsonify(json_data))
-        the_response.status_code = 200
-        the_response.mimetype = 'application/json'
-    return the_response
-    
 
 
 # gets 
